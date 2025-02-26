da4483f5d000ce3908aeca2f53d8541565ca7ae3
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2025-02-26 08:33:35 -0800

    added maze to make_gifs.sh and also updated paths

--- make_gifs.sh
+++ make_gifs.sh
@@ -3,6 +3,8 @@
 # Read the first argument: num environments
 num_envs=$1
 
-python group_images_by_env.py data/coinrun/images gifs/normal $num_envs gifs/normal.tex
-python group_images_by_env.py data/coinrun_aisc/images gifs/aisc $num_envs gifs/aisc.tex
+python group_images_by_env.py data/coinrun gifs/coinrun $num_envs gifs/coinrun.tex
+python group_images_by_env.py data/coinrun_aisc gifs/coinrun_aisc $num_envs gifs/coinrun_aisc.tex
+python group_images_by_env.py data/maze gifs/maze $num_envs gifs/maze.tex
+python group_images_by_env.py data/maze_aisc gifs/maze_aisc $num_envs gifs/maze_aisc.tex
 zip -r gifs.zip gifs
